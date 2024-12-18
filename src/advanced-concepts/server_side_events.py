52c57f9d465f96a645edcd82f01fcc65ea1bc03a
Vishal Kotak <vishal.sanjiv.kotak@gmail.com>
2024-12-17 23:00:22 -0500

    doc: adding comments.

--- src/advanced-concepts/server_side_events.py
+++ src/advanced-concepts/server_side_events.py
@@ -10,12 +10,16 @@ app = FastAPI()
 
 async def get_sse_data():
     for i in range(10):
+        # Adding sleep to view the stream on the browser.
         await asyncio.sleep(2)
         yield f"{time.time()}\n\n"
 
 
 @app.get("/stream")
 async def stream():
+    # StreamingResponse is a construct provided by FastAPI.
+    # text/event-stream informs the browser that it is a stream response 
+    # so do not close the connection.
     return StreamingResponse(get_sse_data(), media_type="text/event-stream")
 
 
