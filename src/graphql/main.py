32d2901d88df50278be1e4620d385634bd732080
Vishal Kotak <vishal.sanjiv.kotak@gmail.com>
2025-04-05 13:48:30 -0400

    feat: GraphQL types and fields.

--- src/graphql/main.py
+++ src/graphql/main.py
@@ -1,17 +1,36 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, Int, Field
+
+
+class UserType(ObjectType):
+    id = Int()
+    name = String()
+    age = Int()
+
 
 class Query(ObjectType):
-    hello = String(name=String(default_value="world"))
+    user = Field(UserType, user_id=Int())
 
-    def resolve_hello(self, info, name):
-        return f"Hello {name}"
-    
+    users = [
+        {'id':1, 'name': 'Test1', 'age':30},
+        {'id':2, 'name': 'Test2', 'age':31},
+        {'id':3, 'name': 'Test3', 'age':32},
+        {'id':4, 'name': 'Test4', 'age':33}
+    ]
+
+    def resolve_user(self, info, user_id):
+        matched_users = [user for user in Query.users if user['id'] == user_id]
+        return matched_users[0] if matched_users else None
+
 
 schema = Schema(query=Query)
 
 gql = '''
-{
-    hello(name: "graphql")
+query {
+    user(userId: 2) {
+        id
+        name
+        age
+    }
 }
 '''
 
